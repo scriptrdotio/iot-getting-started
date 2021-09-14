b1756ab6ac28b7f9e1fabb0e6a07eec2bd6b4f1e
maxwildcat <maxwildcat@gmail.com>
2021-09-15 01:23:40 +0300

    changed the word smart kitchen to smart farming in title

--- login/view/javascript/constants.js
+++ login/view/javascript/constants.js
@@ -1,5 +1,5 @@
 myApp.constant("constants", {
-  appTitle: "Smart Kitchen",
+  appTitle: "Smart Farming",
   login: {
     "background": '//s3.amazonaws.com/scriptr-cdn/smart-farming/background2.jpg', 
     "logo": '//s3.amazonaws.com/scriptr-cdn/smart-farming/logo.jpg',
